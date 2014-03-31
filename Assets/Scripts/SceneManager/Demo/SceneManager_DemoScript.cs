c093a11e8b4d6cc4354ba07ec11652bdf61edf20
Gris87 <Gris87@yandex.ru>
2014-03-31 10:01:09 +0400

    Demo

---
+++ Assets/Scripts/SceneManager/Demo/SceneManager_DemoScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SceneManager_DemoScript : MonoBehaviour
+{
+	private string argument="Test";
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height*0.5f-60, 200, 20), "Argument:");
+		argument=GUI.TextField(new Rect(Screen.width*0.5f-100, Screen.height*0.5f-30, 200, 20), argument);
+
+		if (GUI.Button(new Rect(Screen.width*0.5f-50, Screen.height*0.5f, 100, 20), "Load scene"))
+		{
+			Dictionary<string, string> arguments=new Dictionary<string, string>();
+			arguments["argument"]=argument;
+			SceneManager.LoadScene("SceneManager_Demo2", arguments);
+		}
+	}
+}
---
+++ Assets/Scripts/SceneManager/Demo/SceneManager_DemoScript2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager_DemoScript2 : MonoBehaviour
+{
+	private string argument;
+
+	// Use this for initialization
+	void Start()
+	{
+		argument=SceneManager.sceneArguments["argument"];
+	}
+
+	void OnGUI()
+	{
+		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height*0.5f-30, 200, 20), "Argument:");
+		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height*0.5f, 200, 20), argument);
+	}
+}
