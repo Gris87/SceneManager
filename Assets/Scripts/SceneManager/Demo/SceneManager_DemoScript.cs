915842f4ad6d36fbc6d4febf62148199a2f4c59e
Gris87 <Gris87@yandex.ru>
2014-03-31 23:28:34 +0400

    Small fix

--- Assets/Scripts/SceneManager/Demo/SceneManager_DemoScript.cs
+++ Assets/Scripts/SceneManager/Demo/SceneManager_DemoScript.cs
@@ -8,12 +8,12 @@ public class SceneManager_DemoScript : MonoBehaviour
 
 	void OnGUI()
 	{
-		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height*0.5f-60, 200, 20), "Argument:");
-		argument=GUI.TextField(new Rect(Screen.width*0.5f-100, Screen.height*0.5f-30, 200, 20), argument);
+		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height-90, 200, 20), "Argument:");
+		argument=GUI.TextField(new Rect(Screen.width*0.5f-100, Screen.height-60, 200, 20), argument);
 
-		if (GUI.Button(new Rect(Screen.width*0.5f-50, Screen.height*0.5f, 100, 20), "Load scene"))
+		if (GUI.Button(new Rect(Screen.width*0.5f-50, Screen.height-30, 100, 20), "Load scene"))
 		{
-			Dictionary<string, string> arguments=new Dictionary<string, string>();
+			Dictionary<string, object> arguments=new Dictionary<string, object>();
 			arguments["argument"]=argument;
 			SceneManager.LoadScene("SceneManager_Demo2", arguments);
 		}
--- Assets/Scripts/SceneManager/Demo/SceneManager_DemoScript2.cs
+++ Assets/Scripts/SceneManager/Demo/SceneManager_DemoScript2.cs
@@ -8,12 +8,17 @@ public class SceneManager_DemoScript2 : MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
-		argument=SceneManager.sceneArguments["argument"];
+		argument=(string)SceneManager.sceneArguments["argument"];
 	}
 
 	void OnGUI()
 	{
-		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height*0.5f-30, 200, 20), "Argument:");
-		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height*0.5f, 200, 20), argument);
+		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height-90, 200, 20), "Argument:");
+		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height-60, 200, 20), argument);
+
+		if (GUI.Button(new Rect(Screen.width*0.5f-50, Screen.height-30, 100, 20), "Back"))
+		{
+			SceneManager.LoadScene("SceneManager_Demo");
+		}
 	}
 }
