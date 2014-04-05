59c239bf4c6a35dd516f805b065a890a542070ca
Gris87 <Gris87@yandex.ru>
2014-04-06 01:43:29 +0400

    Intent

--- Assets/Scripts/SceneManager/Demo/SceneManager_DemoScript.cs
+++ Assets/Scripts/SceneManager/Demo/SceneManager_DemoScript.cs
@@ -1,20 +1,20 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class SceneManager_DemoScript : MonoBehaviour
 {
-	private string argument="Test";
+    private string argument="Test";
 
-	void OnGUI()
-	{
-		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height-90, 200, 20), "Argument:");
-		argument=GUI.TextField(new Rect(Screen.width*0.5f-100, Screen.height-60, 200, 20), argument);
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height-90, 200, 20), "Argument:");
+        argument=GUI.TextField(new Rect(Screen.width*0.5f-100, Screen.height-60, 200, 20), argument);
 
-		if (GUI.Button(new Rect(Screen.width*0.5f-50, Screen.height-30, 100, 20), "Load scene"))
-		{
-			Hashtable arguments=new Hashtable();
-			arguments.Add("argument", argument);
-			SceneManager.LoadScene("SceneManager_Demo2", arguments);
-		}
-	}
+        if (GUI.Button(new Rect(Screen.width*0.5f-50, Screen.height-30, 100, 20), "Load scene"))
+        {
+            Hashtable arguments=new Hashtable();
+            arguments.Add("argument", argument);
+            SceneManager.LoadScene("SceneManager_Demo2", arguments);
+        }
+    }
 }
--- Assets/Scripts/SceneManager/Demo/SceneManager_DemoScript2.cs
+++ Assets/Scripts/SceneManager/Demo/SceneManager_DemoScript2.cs
@@ -1,23 +1,23 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class SceneManager_DemoScript2 : MonoBehaviour
 {
-	private string argument;
+    private string argument;
 
-	// Use this for initialization
-	void Start()
-	{
-		argument=(string)SceneManager.sceneArguments["argument"];
-	}
+    // Use this for initialization
+    void Start()
+    {
+        argument=(string)SceneManager.sceneArguments["argument"];
+    }
 
-	void OnGUI()
-	{
-		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height-90, 200, 20), "Argument:");
-		GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height-60, 200, 20), argument);
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height-90, 200, 20), "Argument:");
+        GUI.Label(new Rect(Screen.width*0.5f-100, Screen.height-60, 200, 20), argument);
 
-		if (GUI.Button(new Rect(Screen.width*0.5f-50, Screen.height-30, 100, 20), "Back"))
-		{
-			SceneManager.LoadScene("SceneManager_Demo");
-		}
-	}
+        if (GUI.Button(new Rect(Screen.width*0.5f-50, Screen.height-30, 100, 20), "Back"))
+        {
+            SceneManager.LoadScene("SceneManager_Demo");
+        }
+    }
 }
